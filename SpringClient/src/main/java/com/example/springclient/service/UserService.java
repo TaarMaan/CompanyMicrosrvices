690471a56170f11076cac82547ba5d0345685b26
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-06-21 20:34:01 +0400

    Update UserService.java
    documentation

--- SpringClient/src/main/java/com/example/springclient/service/UserService.java
+++ SpringClient/src/main/java/com/example/springclient/service/UserService.java
@@ -14,38 +14,81 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    /**
+     *
+     * @return Возвращает список пользователей
+     */
     public List<UserDTO> getAll() {
         return userRepository.findAll();
     }
 
+    /**
+     *
+     * @param id идентификатор пользователя в бд
+     * @return Возвращает юзера по id
+     */
     public Optional<UserDTO> getById(long id) {
         return userRepository.findById(id);
     }
 
+    /**
+     *
+     * @param newUser Сущность нового пользователя
+     * @return Сохранение пользователя в БД
+     */
     public UserDTO save(UserDTO newUser) {
         return userRepository.save(newUser);
     }
 
+    /**
+     * Удаление пользователя из БД
+     *
+     * @param id
+     */
     public void delete(long id) {
         userRepository.deleteById(id);
     }
 
+    /**
+     *
+     * @param username Имя пользователя
+     * @return Возвращает пользователя по имени
+     */
     public UserDTO findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
+    /**
+     *
+     * @param id
+     * @return Показывает пользователя по id
+     */
     public boolean findByUserId(long id) {
         return userRepository.findById(id).isPresent();
     }
 
+    /**
+     *
+     * @param id
+     * @return Возвращает пользователя по id
+     */
     public UserDTO getByUserId(long id) {
         return userRepository.getOne(id);
     }
 
+    /**
+     *
+     * @param companyId Идентификатор компании
+     * @return Возвращает компанию по id
+     */
     public List<UserDTO> findByCompanyId(Integer companyId) {
         return userRepository.findByCompany_id(companyId);
     }
 
+    /**
+     *
+     * @param id Обновляет данные компании по id
+     */
     public void updateCompany(Integer id) {
         userRepository.updateCompany_id(id);
     }
