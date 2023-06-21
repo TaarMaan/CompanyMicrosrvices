7df97ad2e70f7e5c5439671a6a36632396a8ce7f
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-06-21 20:33:10 +0400

    Update SimpleClient.java
    +documentation

--- SpringClient/src/main/java/com/example/springclient/SimpleClient.java
+++ SpringClient/src/main/java/com/example/springclient/SimpleClient.java
@@ -4,12 +4,25 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Интерфейс для реализации клиента и запросов по id компаний
+ */
 @FeignClient(name = "Companies", url = "http://localhost:8099")
 public interface SimpleClient {
 
+    /**
+     * Получить информацию о компании по ее идентификатору.
+     *
+     * @param companyId Идентификатор компании.
+     */
     @GetMapping("/companies/{companyId}")
     boolean findById (@PathVariable Integer companyId);
 
+    /**
+     * Получить информацию о конкретной компании по ее идентификатору.
+     *
+     * @param companyId Идентификатор компании.
+     */
     @GetMapping("/companies/getOne/{companyId}")
     String getOne(@PathVariable Integer companyId);
 }
