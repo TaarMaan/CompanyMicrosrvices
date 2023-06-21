7c50500700d4b7c887c589b4214ee640d62b2fd6
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-06-21 20:34:59 +0400

    Update UserRepository.java

--- SpringClient/src/main/java/com/example/springclient/repository/UserRepository.java
+++ SpringClient/src/main/java/com/example/springclient/repository/UserRepository.java
@@ -8,13 +8,30 @@ import org.springframework.data.jpa.repository.Query;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * Репозиторий для взаимодействий сущностей пользователь и компания
+ */
 public interface UserRepository extends JpaRepository<UserDTO, Long> {
+    /**
+     *
+     * @param username Имя пользователя
+     * @return Возвращает пользователя по имени
+     */
     UserDTO findByUsername(String username);
 
+    /**
+     *
+     * @param companyId Идентификатор компании
+     * @return Возвращает список пользователей по id у которых companyId совпадают
+     */
     @Modifying
     @Query(value = "SELECT * FROM users where company_id= :companyId", nativeQuery = true)
     List<UserDTO> findByCompany_id(Integer companyId);
 
+    /**
+     *
+     * @param id удаляет компанию у вабранного по id пользоваотеля
+     */
     @Modifying
     @Transactional
     @Query(value = "UPDATE users SET company_id = null WHERE id = :id", nativeQuery = true)
