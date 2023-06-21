708ced6e396f5daed7e30b54fc275c040f8ac4ce
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-06-21 12:47:55 +0400

    +submodules microservices

---
+++ SpringClient/src/main/java/com/example/springclient/SpringClientApplication.java
@@ -0,0 +1,22 @@
+package com.example.springclient;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientApplication.class, args);
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+}
