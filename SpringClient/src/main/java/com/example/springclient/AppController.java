16fc0860b1a09c84f8e3811349c1b7b7e59bbbed
Alester <90714403+TaarMaan@users.noreply.github.com>
2023-06-21 20:32:00 +0400

    Update AppController.java
    documentation

--- SpringClient/src/main/java/com/example/springclient/AppController.java
+++ SpringClient/src/main/java/com/example/springclient/AppController.java
@@ -5,17 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Контроллер для обработки HTTP-запросов в модуле SpringClient.
+ */
 @RestController
 @RequiredArgsConstructor
 public class AppController {
 
     private final SimpleClient client;
 
+    /**
+     * Реализация метода @findById из интерфейса SimpleClient
+     *
+     * @param companyId Идентификатор компании.
+     * @return Информация о компании.
+     */
     @GetMapping("/companies/{companyId}")
     public boolean findById(@PathVariable Integer companyId) {
         return client.findById(companyId);
     }
 
+    /**
+     * Реализация метода @getOne из интерфейса SimpleClient
+     *
+     * @param companyId Идентификатор компании.
+     * @return Информация о компании.
+     */
     @GetMapping("/companies/getOne/{companyId}")
     public String getOne(@PathVariable Integer companyId) {
         return client.getOne(companyId);
